9d0ce8ec18b702087d0de93a7a7e715f5073114a
Jay Price <jared.x.price@gmail.com>
2016-12-04 16:43:58 -0500

    Output added.

--- YTtripsdata/hadoop2016.sh
+++ YTtripsdata/hadoop2016.sh
@@ -35,10 +35,4 @@ cat tmpfile >> output
 rm tmpfile
 hdfs dfs -rm -r output
 
-hadoop jar /home/jay/Github/applied_stats/YTtripsmapreduce/target/YTtrips-1.0.jar YTtrips /user/jay/input/yellow_tripdata_2016-07.csv output
-hdfs dfs -get output/part-r-00000 tmpfile
-cat tmpfile >> output
-rm tmpfile
-hdfs dfs -rm -r output
-
 
